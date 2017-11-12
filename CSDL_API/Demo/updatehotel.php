f7feb3f046909ff861c3de1337a525b0a330ff59
VoTuanNguyen <14110133@student.hcmute.edu.vn>
2017-11-12 11:40:42 +0700

    Cập nhật khách sạn

--- CSDL_API/Demo/updatehotel.php
+++ CSDL_API/Demo/updatehotel.php
@@ -18,27 +18,47 @@ $tiennghi = $obj['tiennghi'];
 $website = $obj['website'];
 //decode dataimg và up ảnh lên host
 $data = base64_decode($dataimg);
-//set nơi lưu vào và tên file.png
-$uri = '';
-if($data){
-	$file = 'images/'. uniqid() . '.png';
-	$success = file_put_contents($file, $data);
-	$uri = $myhost.$file;
-}
-// echo $userid.' '.$namehotel.' '.$price.' '.$hoteltype.' '.$phone.' '.$date.' '.$address.' '.$lat.' '.$lng.' '.$website.' '.$uri;
-if($namehotel !=='' && $price !== '' && $dataimg!=='' && $hoteltype !=='' && $phone !== '' && $date!=='' && $address !=='' && $lat !== '' && $lng!=='' && $tiennghi !== '' && $uri !== ''){
-	
- 	$sql = "UPDATE khachsan SET ten = '$namehotel', gia = '$price', hinhanh = '$uri', diachi = '$address', loai = '$hoteltype', website = '$website', sdt = '$phone', tiennghihangdau = '$tiennghi', ngaycapnhat = '$date', lat = '$lat', `long` = '$lng' WHERE id = '$id'";
-	$result = $mysqli->query($sql);
-	if($result){
-		echo 'THANH_CONG';
+//Nếu không cập nhật ảnh
+if($dataimg === true){
+	if($namehotel !=='' && $price !== '' && $hoteltype !=='' && $phone !== '' && $date!=='' && $address !=='' && $lat !== '' && $lng!=='' && $tiennghi !== ''){
+		
+	 	$sql = "UPDATE khachsan SET ten = '$namehotel', gia = '$price', diachi = '$address', loai = '$hoteltype', website = '$website', sdt = '$phone', tiennghihangdau = '$tiennghi', ngaycapnhat = '$date', lat = '$lat', `long` = '$lng' WHERE id = '$id'";
+		$result = $mysqli->query($sql);
+		if($result){
+			echo 'THANH_CONG';
+		}
+		else{
+			echo 'KHONG_THANH_CONG';
+		}
 	}
 	else{
-		echo 'KHONG_THANH_CONG';
+			echo 'KHONG_THANH_CONG';
 	}
 }
-else{
-		echo 'KHONG_THANH_CONG';
+else{ //Có cập nhật ảnh đại diện
+	//set nơi lưu vào và tên file.png
+	$uri = '';
+	if($data){
+		$file = 'images/'. uniqid() . '.png';
+		$success = file_put_contents($file, $data);
+		$uri = $myhost.$file;
+	}
+	// echo $id.' '.$namehotel.' '.$price.' '.$hoteltype.' '.$phone.' '.$date.' '.$address.' '.$lat.' '.$lng.' '.$website.' '.$uri;
+
+	if($namehotel !=='' && $price !== '' && $dataimg!=='' && $hoteltype !=='' && $phone !== '' && $date!=='' && $address !=='' && $lat !== '' && $lng!=='' && $tiennghi !== '' && $uri !== ''){
+		
+	 	$sql = "UPDATE khachsan SET ten = '$namehotel', gia = '$price', hinhanh = '$uri', diachi = '$address', loai = '$hoteltype', website = '$website', sdt = '$phone', tiennghihangdau = '$tiennghi', ngaycapnhat = '$date', lat = '$lat', `long` = '$lng' WHERE id = '$id'";
+		$result = $mysqli->query($sql);
+		if($result){
+			echo 'THANH_CONG';
+		}
+		else{
+			echo 'KHONG_THANH_CONG';
+		}
+	}
+	else{
+			echo 'KHONG_THANH_CONG';
+	}
 }
 
 ?>
